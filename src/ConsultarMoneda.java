26120df357713c18858bbc6c578522de9b9deff7
Ahinardev <ahinar@gmail.com>
2024-05-08 12:32:14 -0500

    Add files via upload

---
+++ src/ConsultarMoneda.java
@@ -0,0 +1,28 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultarMoneda {
+    public Monedas buscarMoneda(String monedaBase, String monedaTarget){
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/a519b45e35b5a17a8b95bec2/pair/"+monedaBase+"/"+monedaTarget);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Monedas.class);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("No encontre la Película");
+        }
+    }
+
+}
